c305d0f465a4e764f5ee2c6cd658b944d83b0575
91998 <91998@view-localhost>
2022-09-13 20:01:31 +0530

    5commit

---
+++ Hello/src/com/coreprograms/FinalDemo.java
@@ -0,0 +1,32 @@
+package com.coreprograms;
+
+public class FinalDemo {
+
+	public static void main(String[] args) {
+
+		final int a=25;   // constant
+		
+				
+	}
+	
+	
+	class Parent
+	{
+		void property()
+		{
+			System.out.println("House, property,wealth");
+		}
+		
+	}
+	
+	class Child extends Parent
+	{
+		void marry()
+		{
+			
+		}
+		
+		
+	}
+
+}
